88832550a162b47da243b6afe209210f15d9ef0c
rakibislamdev <rakibislamdev@gmail.com>
2022-06-07 10:04:51 +0600

    some new style added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,20 +4,21 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>EdiTe pAgE</title>
+    <title>ToDo aPP - welcome to our todo app</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 
 <body>
     <!-- table section-->
-    <section class="m-5">
+    <section class="m-5 p-4 shadow mb-5 bg-body rounded">
+        <h1 class="text-center p-3">Our Todo List</h1>
         <table class="table table-bordered border-primary">
             <thead>
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Work</th>
-                    <th scope="col">Descripton</th>
+                    <th scope="col">Work Detealis</th>
                     <th scope="col">Date</th>
                     <th scope="col">Action</th>
                 </tr>
@@ -29,8 +30,8 @@
                     <td>Otto</td>
                     <td>@mdo</td>
                     <td>
-                        <button>edit</button>
-                        <button>delete</button>
+                        <button type="button" class="btn btn-primary">Edit</button>
+                        <button type="button" class="btn btn-danger">Delete</button>
                     </td>
                 </tr>
                 <tr>
@@ -39,8 +40,8 @@
                     <td>Thornton</td>
                     <td>@fat</td>
                     <td>
-                        <button>edit</button>
-                        <button>delete</button>
+                        <button type="button" class="btn btn-primary">Edit</button>
+                        <button type="button" class="btn btn-danger">Delete</button>
                     </td>
                 </tr>
             </tbody>
